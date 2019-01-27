28abe763843ece7fc93ae200cbdcf381943c4171
Naftali <weiss@Naftalis-iMac.fios-router.home>
2019-01-26 19:25:20 -0500

    made a movie Schema

---
+++ models/movies.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const movieSchema = new mongoose.Schema({
+  title: {type: String, required:true},
+  rating:{type: Number, min:1 , max:5},
+  writer:String,
+  starring:[String],
+  img:[String],
+  catigory:[String],
+  favoites:Boolean,
+  runtime:{type:Number, default:130},
+  movieDescription: String,
+  addedBy:String,
+  year: Number
+})
+
+const Movie = mongoose.model('movies', movieSchema);
+module.exports = Movie;
